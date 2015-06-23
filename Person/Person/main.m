56fab1a5f4438dafc6ae116bc79d92de74048f7d
Daniel Distant <daniel.distant1@gmail.com>
2015-06-22 20:26:46 -0400

    First and last commit because I like to live dangerously

--- Person/Person/main.m
+++ Person/Person/main.m
@@ -19,6 +19,10 @@
 - (void)setPhoneNumber:(NSString *)phoneNumber;
 - (NSString *)phoneNumber;
 
+- (BOOL)checkSameCity:(Person *)person;
+
+- (Person *)registerChild;
+
 @end
 
 @implementation Person {
@@ -51,13 +55,77 @@
     return _phoneNumber;
 }
 
+- (BOOL)checkSameCity:(Person *)person {
+    
+    if ([[person city] isEqualToString:[self city]]) {
+        
+        return YES;
+        
+    } else {
+        
+        return NO;
+    }
+}
+
+- (Person *)registerChild {
+    
+    Person *child;
+    
+    child = [[Person alloc] init];
+    
+    [child setPhoneNumber:[self phoneNumber]];
+    
+    [child setCity:[self city]];
+    
+    return child;
+    
+}
+
 @end
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSLog(@"This is Robert.");
+        
+        Person *person = [[Person alloc] init];
+        [person setName:@"Robert"];
+        
+        NSLog(@"He lives in New York City and has his own phone number.");
+        [person setCity:@"New York City"];
+        [person setPhoneNumber:@"718-123-4567"];
+        
+        NSLog(@"Robert met Jane.");
+        
+        Person *person2 = [[Person alloc] init];
+        [person2 setName:@"Jane"];
+        [person2 setCity:@"Chicago"];
+        
+        BOOL result = [person checkSameCity:person2];
+        
+        NSLog(@"Does she live in the same city as him? %s", result ? "Yes!":"No...");
+        
+        NSLog(@"Then Robert met Vanessa. She has a phone number too. Do they live in the same city?");
+        
+        Person *person3 = [[Person alloc] init];
+        [person3 setName:@"Vanessa"];
+        [person3 setPhoneNumber:@"718-123-4567"];
+        [person3 setCity:@"New York City"];
+        
+        BOOL result2 =[person checkSameCity:person3];
+        
+        NSLog(@"%s", result2 ? "Yes!":"No...");
+        
+        NSLog(@"Robert and Vanessa have a child named Bunny who has the same phone number and city as they do.");
+        
+        Person *child = [person registerChild];
+        
+        [child setName:@"Bunny"];
+        BOOL result3 = [child checkSameCity:person];
+        
+        NSLog(@"%s", result3 ? "Yes!":"No...");
+        
     }
     return 0;
 }
